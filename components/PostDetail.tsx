9468038b67052d5dd3df338a582776bc9298d36e
Bini Tek <bini.tek1@gmail.com>
2022-12-28 12:48:20 +0000

    updte

--- components/PostDetail.tsx
+++ components/PostDetail.tsx
@@ -50,7 +50,7 @@ const PostDetail = ({ post }: any) => {
         return (
           <p
             key={index}
-            className='text-md text-zinc-800 leading-normal mb-3 px-6'>
+            className='text-md text-zinc-800 leading-normal mb-5 px-6'>
             {modifiedText.map((item: any, i: any) => (
               <React.Fragment key={i}>{item}</React.Fragment>
             ))}
@@ -94,12 +94,19 @@ const PostDetail = ({ post }: any) => {
 
   return (
     <div className='bg-white shadow-sm rounded-sm pb-12 mb-8 mt-8 lg:mt-0'>
-      <div className='px-0 lg:px-0 pt-5'>
+      <div className='px-0 lg:px-0'>
+        <div className='relative overflow-hidden shadow-sm mt-1 mb-6'>
+          <img
+            src={post.featuredImage.url}
+            alt={post.title}
+            className='object-top h-full w-full'
+          />
+        </div>
         <p className='px-6 uppercase text-xs mb-1'>{post.categories[0].name}</p>
         <h1 className='px-6 text-2xl lg:text-3xl text-gray font-semibold mb-2 tracking-normal font-inter'>
           {post.title}
         </h1>
-        <p className='px-6 text-zinc-800'>{post.excerpt}</p>
+        <p className='px-6 text-zinc-800 text-sm'>{post.excerpt}</p>
         <div className='px-6 pt-3 flex flex-row'>
           <div className='font-medium mb-1'>
             <span className='text-sm mb-10 text-zinc-800'>
@@ -138,13 +145,8 @@ const PostDetail = ({ post }: any) => {
             </EmailShareButton>
           </div>
         </div>
-        <div className='relative overflow-hidden shadow-sm mt-3 mb-6'>
-          <img
-            src={post.featuredImage.url}
-            alt={post.title}
-            className='object-top h-full w-full'
-          />
-        </div>
+
+        <hr className='mb-3 mx-6' />
 
         {post.content.raw.children.map((typeObj: any, index: any) => {
           const children = typeObj.children.map((item: any, itemIndex: any) =>
